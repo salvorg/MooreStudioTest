8e2d80baab47e4305ae2dd32d7b94ff1bd9489ba
Almaz Balbaev <kujitengu@gmail.com>
2025-01-30 23:08:21 +0600

    added custom font size, colors * background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,22 @@ module.exports = {
             fontSize: {
                 '18': '18px',
                 '32': '32px',
+                '52': '52px',
+                '56': '56px',
             },
             colors: {
                 txtNavbar: "#2F2F2F",
                 txtTitle: "#2E2E2E",
                 txtOlive: "#93A27C",
                 txtLightBrown: "#B89776",
+                txtGrey: "#797979",
+                cream: "#FAF5F1",
+                coffee: "#493829",
+                sand: "#B89776",
             },
             backgroundImage: {
                 'hero-bg': "url('../public/hero-section-bg.jpg')",
+                'about-bg': "url('../public/about-bg.jpeg')",
             },
         },
     },
