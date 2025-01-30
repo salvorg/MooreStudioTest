341aa39a5980b94db804b93063bf5fc8cc9b87fe
Almaz Balbaev <kujitengu@gmail.com>
2025-01-30 23:08:34 +0600

    added mok data

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,58 @@
 // export const apiURL = '/api/';
-export const apiURL = 'http://localhost:5000/api/';
-export const GOOGLE_CLIENT_ID = '81999010408-m036rr97tcfltkl9ctot9bedutjnhqcb.apps.googleusercontent.com';
-export const HEADER_HEIGHT = '72px';
-export const FOOTER_HEIGHT = '138px';
+import {Cards, RecommendedCard} from "./types";
+
+export const cards: Cards[] = [
+    {
+        id: 1,
+        image: 'https://s3-alpha-sig.figma.com/img/e649/b047/d4b9fa3ef8f585fceb6c47041d15be0f?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ALkrGOcKNbQEh8RNvY5t~bEDm-e23qKsKGKwUZ2SkyOBRQOSPYYKsUaP1lefjFZ5BqCUECi9VTS17e8Uf1VySbdhZx7n4wjeVeWW9R0l39CSxDamTCq2L8KerEJmuPhSFadcmdq49aWIzWSUpguzIx~7fkoDkLno4zwwY1zA39sdTh8AxrmByQdThMrwl8dD2-93REHfj1tXcFCmDl-yRt3OZA2k9mH1pCOu-j5p7qbEqnG~qy6l5JtojUBxonu0LFgOLtLmlF2pxpF0a~hgIAUgbOZmPzjFFd6SS3ATJSCgVnGG9MYh6EBF3JfmagNjQwYLQ4JT6LqpE6q8-Ez~2w__',
+        title: 'Шоколад и какао продукты',
+    },
+    {
+        id: 2,
+        image: 'https://s3-alpha-sig.figma.com/img/b9c5/86c2/312d426d5f6b3bf38093495e864632a6?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pkLpZrdMpSS6ovdm6O3mSkRr9ltbJ9PH~uxK1GG39yWPBmtia8TC~p7CZWCaAfNVdvKQB~u45UlrJCDHRP49dqSUpPBAi1WKoad05wwnDhneYR0-MCrVsqjB1zp2Sjhd52-oSXJ-a7avfCEvH4AP8uqS-tHzxNW6VIxn9f6RbcBnrc9mPf7~NsED98m3YwO7jjMozr-~ZC6uAs4lK1oj8DgTSC2fVzQgZGgr~Q5S4R1hP1DDVnfZugh69C16C2RG6zV-wJh~ki9AQ1Oz5hBQ6pv~zPATbsxwJabKc-tYacmv~2Kfi6cGHoHpVsWfZKHWP-Y~DPe1wzEZy1-N2pW3FQ__',
+        title: 'Молочная продукция',
+    },
+    {
+        id: 3,
+        image: 'https://s3-alpha-sig.figma.com/img/1d92/9285/01907b955eaa7db4665d8a6963626c30?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UMBwxunQ9HPQDj90gW-v4lzqHWYqyABpPq9eBwg4HMg9QuFnF9nrsLVeHtiyZ1NblthE0oU4FkQELBoqDmF1N~7QkOfJNeo-j7rFWq9XzWEFtsRr8OPiwrpkwA0HMEQjw-1WXAM5MVQFJINOXky1XN36TQYh8X5NRVNrVId0owzfN20aqCvfvpwrN6ayUdxiX-3-GkhvYmSMBzUhlmQKSXZLwOpa6bweYReFVS4XHdIJkM2H5t998zLev-e9pxYPdYMmD9Mj1xzfzsiIo4ypDOMLJFO8Nc8v5VdrSfa6buefkTe1qfMkF~X6-fUGx2o~44AENZNGq1vriLMnIFx3zA__',
+        title: 'Ингредиенты',
+    },
+    {
+        id: 4,
+        image: 'https://s3-alpha-sig.figma.com/img/3559/2b28/49c887beb904f5a833c09427293f34fc?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jjYst~b7o2ZhjdopPNCjr3u0Sn9bhsAYbfl-wx9V1Emzg2yGaWYIO2TS7kTjCEYJftkVGBERzFwkm7E3BInXk6Udu6zIzj-lfjQmx74SXW1d~bBvpaApN6sxHH-GxWtb3Qu3FTSljEoVYA-1tTOYJgjYDUVz57PoqOe64cq5yAYNLR-XSXuEHChzXSk~VeHNRw6UETlhLtwSZgJZWZZSNsrJYaaKHfoBSGnsTOaWMzHnRsUFtEzamN2ykSqvNrgnslKRSfVcSOVhXBULdAM~tSR7YD20nQ3V3J1aEk0slBYRn7amL8ydX9cdbczqhuxEZAy2B93JEcQl0k9-HEcy5w__',
+        title: 'Покрытия и наполнители',
+    },
+];
+
+export const recommended: RecommendedCard[] = [
+    {
+        id: 1,
+        image: 'https://s3-alpha-sig.figma.com/img/bd2d/8030/3984f15e2a0e11db5241b902346a8de2?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eYkdmanetrpZFKrwrIrbXmLhCSQ810F2lMYJHaY3KDR5W9OZvIz3l5orlMRCWuM~eNYfhWU7v6ACUmW~rUQMCI8XvxkU9Khm7sBMfr5n5vBc2mphLuNK4DiNCuHsksmT5FDQWb1ZO6LanR9--d3FVzWIOgjM0AuQflWWZ5vnZpS2we4zM9t9tKdm5828sFGHN1tdG004Uc5bZIc3yctzcGMwqNWJpBS-jUqB0MYvKX2scf1BjiFXq6WfTBj~4Q3p6gc7A2sWH8F0fGxwyOMvoi0QMGg9sUfHBKFINnptnPh6R-2IxPoRy2-xMQAy-1pRrXSJzkCdUirKveYFgEbjcQ__',
+        description: 'Шпатель кондитерский пластиковый 254мм 50 SG250B',
+        weight: 150,
+        price: 550,
+    },
+    {
+        id: 2,
+        image: 'https://s3-alpha-sig.figma.com/img/ee6c/12aa/f7c690f164f5806f300565a52f43370d?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bQxie4bCeN38WxqoPe4Jl2RBUiW22vkeLY8a8nIcIP5sjnHR9vFWZH6x2EFSCN4cRp0SHHtsOZZSM1lGQh9oqbcp3dPfQHTCpinSlk404Id21LAcofuYLhAbasbm4dP8u8Mue0BoHiN2NkymD2Sro2e2WpZcOO6suZoXrwpFady5UEp6wZZ0sRWSvKaq2imDy2xG~SQYluAaXCY2pRDs4PeMgiN8z21eydK61ksLXkkRctO-ARES2m6fHxc89VBW1avuy0P9fYLYMUpB877DNiwM9yhV6n2ECwzUjF4d2jPNUWAiSLeRhTlYbwktgYh2KcZOcTgbboNiZlie0UM-qw__',
+        description: 'Шпатель кондитерский пластиковый 254мм 50 SG250B',
+        weight: 150,
+        price: 550,
+    },
+
+    {
+        id: 3,
+        image: 'https://s3-alpha-sig.figma.com/img/b610/914e/7a69dca5a6042e74510d607a61165fb7?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hSdPVrwOujGNiFOgr0KuTwn4s4t~dpshY2w37WzZgptDAnRsmP6-6LV33ifiRT2WEeo4yOKz5IL5E74tjoVDNYzvMJoYGQwlK~MSFC2fJB2k9BjzPdfZc4MtiR8GZ-SP4l9obuWmdrIcDLcyehKwWOHRTJdjmjGCxC8b1FlaMdeBQSlttzdLeSu4kh-MTwUNvjrfakZFDyASddamWDr8q1tcJ89jzd3K4uU8Z6EBdiD4UsYsaIyAWU~3EFig1Hf7hWRtKYMbuIjIqaGEtXLaTXrGcF7R-7GaaudRv1jqgvhnNO9nUoNtw5fu1VwEZc2uXcdgjUflYmbEFvcXs7-j5Q__',
+        description: 'Шпатель кондитерский пластиковый 254мм 50 SG250B',
+        weight: 150,
+        price: 550,
+    },
+
+    {
+        id: 4,
+        image: 'https://s3-alpha-sig.figma.com/img/429e/c9a0/7ea68520819b7ebcb723fabd46dfe752?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VdjkakNe7trVklausQgoZGjxvAbZZNgAcBUOsu208w3oUqwQdRmxcNQSAYscdDxHXZOJqeM2jcdPIWO-YOf2e5nlDBNYDK0YlOFhKNzYVw1k6WVzu5wEjR57U7Jzn3mveaQ079lcReFOPK9rEuCwGoKokMwjUVa4YrhNn4~-90ZNBOTxvylgdktrf2OHPmdDp01rdIAsrbcrWIaEh4r9LwQrJ0L7kYIbndCBNG~FKHHAOBAkQSMlCTnZ-6ZASKJbMuLMF7HVXPBnlgC8vnn0PRtWDMsQ4-r-3IrH02xE7Q~42FwAM4rfLg7-ly0yh70R518yIupvAG0dHijuzytt2Q__',
+        description: 'Шпатель кондитерский пластиковый 254мм 50 SG250B',
+        weight: 150,
+        price: 550,
+    },
+]
