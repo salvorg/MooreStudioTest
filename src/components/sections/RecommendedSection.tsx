a8a2e7c771996021bd9c890dcdd6267ff030e716
Almaz Balbaev <kujitengu@gmail.com>
2025-01-31 01:32:43 +0600

    changed background

--- src/components/sections/RecommendedSection.tsx
+++ src/components/sections/RecommendedSection.tsx
@@ -6,7 +6,7 @@ import LandingButton from "../ui/buttons/LandingButton";
 
 const RecommendedSection = () => {
     return (
-        <div className="bg-cream">
+        <div className="bg-cream2">
             <div className="max-w-container mx-auto py-20">
                 <p className="text-txtOlive text-lg font-medium font-montserrat">Вам могут понравится</p>
                 <LandingTitle className="relative text-56 text-txtTitle mt-2">
