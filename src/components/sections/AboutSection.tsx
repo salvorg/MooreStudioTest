0b41607bdbba64859d235cc879be1c226aad64a3
Almaz Balbaev <kujitengu@gmail.com>
2025-01-31 12:33:26 +0600

    changed bg

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -18,7 +18,8 @@ const AboutSection = () => {
                         </p>
                     </div>
                     <div
-                        className="relative w-[742px] h-[503px] bg-about-bg bg-cover rounded-xl"
+                        className="relative w-[742px] h-[503px] bg-cover bg-center rounded-xl"
+                        style={{ backgroundImage: `url(https://s3-alpha-sig.figma.com/img/9d54/703a/141a1e8a566f098a682e3d97d74a1fe0?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=MRb-1cObitVXXh5soPh25027jAXhX7e8Mo4LxcwgdomHhi0IfIDbijrm2XzLno~9EhowuhL0fa20tzoKCbbXGeJdLoDWgm3wh2L3lPL~oHmkhZJ0shTkzAXT3m-oMvn2cq~zjFwPrSiU2xHFvZ~EonqTU~UrpUaIqZDmsHiaPHOqtw7u3eFe00TNthC~Xo14DtWZVKe9mE5O05kQdKo-mmHw0bC~2Cpm5mvuGedQZ1je5M4LdhJbCnkxUCCpey2vpLVmiDm~IStY619Rf5sgX452VQb8iwucdO2JxOAl7BDYCwguEDMFjk1WHCN0kmvjDRfe7HKnUg5lAp3Hgim3OA__)` }}
                     >
                         <LandingButton className="absolute bottom-8 right-9 ">Читать далее</LandingButton>
                     </div>
