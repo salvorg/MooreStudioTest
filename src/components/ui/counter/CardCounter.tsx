375ab10d8a23bc5a48f7dc4abc6532f84f81be99
Almaz Balbaev <kujitengu@gmail.com>
2025-01-31 12:35:00 +0600

    changed font size

--- src/components/ui/counter/CardCounter.tsx
+++ src/components/ui/counter/CardCounter.tsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 
 const CardCounter = () => {
-    const [quantity, setQuantity] = useState(1);
+    const [quantity, setQuantity] = useState(0);
 
     const handleIncrease = () => setQuantity(prev => prev + 1);
     const handleDecrease = () => {
-        if (quantity > 1) {
+        if (quantity > 0) {
             setQuantity(prev => prev - 1);
         }
     };
@@ -14,14 +14,14 @@ const CardCounter = () => {
         <div className="flex items-center justify-center space-x-2 w-fit p-1 bg-white rounded-full border border-ligthGrey">
             <button
                 onClick={handleDecrease}
-                className="w-6 h-6 bg-grey rounded-full flex items-center justify-center text-xl font-bold"
+                className="w-6 h-6 bg-grey pb-[2px] rounded-full flex items-center justify-center text-xl"
             >
                 -
             </button>
             <span className="text-base">{quantity}</span>
             <button
                 onClick={handleIncrease}
-                className="w-6 h-6 bg-grey rounded-full flex items-center justify-center text-xl font-bold"
+                className="w-6 h-6 bg-grey pb-[3px] rounded-full flex items-center justify-center text-xl"
             >
                 +
             </button>
